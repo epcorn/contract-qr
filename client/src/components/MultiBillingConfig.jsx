1f4cb505b8c76095cbde0292044bc766b74e5008
epcorn <exteam.epcorn@gmail.com>
2025-09-24 15:38:01 +0530

    changes in multibilling

--- client/src/components/MultiBillingConfig.jsx
+++ client/src/components/MultiBillingConfig.jsx
@@ -67,7 +67,7 @@ const MultiBillingConfig = ({ services, startDate, endDate }) => {
     useDataContext();
 
   const billingFrequencyOptions = [
-    "Full payment in advance", // CHANGED: Added new option
+    "Full Payment While Signing The Contract", // CHANGED: Added new option
     "50% while signing contract and 50% after 6 months",
     "Quarterly start",
     "Quarterly end",
